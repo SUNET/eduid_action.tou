6174fa7449fb6c8f8493fde2acfd044395a53d3f
Enrique Pérez Arnaud <enrique@cazalla.net>
2018-07-02 14:35:12 +0200

    paint the tou and button

--- v2/js/component.js
+++ v2/js/component.js
@@ -3,7 +3,18 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import i18n from 'i18n-messages';
+import { appFetching } from "actions/ActionWrapper";
 import ActionWrapperContainer from "containers/ActionWrapper";
+import EduIDButton from "components/EduIDButton";
+
+
+export const TOU_ACCEPTED = "TOU_ACCEPTED";
+
+const acceptedTOU = () => ({
+    type: TOU_ACCEPTED
+});
+
 
 class Main extends Component {
 
@@ -11,19 +22,44 @@ class Main extends Component {
 
         return (
             <ActionWrapperContainer>
-            hello world!
+              <div id="eduid-tou" dangerouslySetInnerHTML={{__html: this.props.tous[this.props.lang]}} />
+              <div id="accept-tou-button">
+                <EduIDButton onClick={this.props.acceptTOU}>
+                  {this.props.l10n('tou.accept')}
+                </EduIDButton>
+              </div>
             </ActionWrapperContainer>
         );
     }
 }
 
 Main.propTypes = {
+    tous: PropTypes.object,
+    lang: PropTypes.string,
+    acceptTOU: PropTypes.func
 }
 
+const mapStateToProps = (state, props) => {
+    return {
+        tous: state.plugin.tous,
+        lang: state.intl.locale
+    }
+};
+
+const mapDispatchToProps = (dispatch, props) => {
+    return {
+        acceptTOU: function (e) {
+            e.preventDefault();
+            dispatch(appFetching());
+            dispatch(acceptedTOU());
+        }
+    }
+};
+
 const MainContainer = connect(
-  () => ({}),
-  () => ({})
+    mapStateToProps,
+    mapDispatchToProps
 )(Main);
 
-export default MainContainer;
+export default i18n(MainContainer);
 
--- v2/js/root-saga.js
+++ v2/js/root-saga.js
@@ -4,11 +4,17 @@ import { put, select } from "redux-saga/effects";
 
 import * as actions from "actions/ActionWrapper";
 import { requestConfig } from "sagas/ActionWrapper";
+import * as component from "./component";
+
+
+function* postAcceptTOU () {
+}
 
 
 function* rootSaga() {
   yield [
     takeLatest(actions.GET_ACTIONS_CONFIG, requestConfig),
+    takeLatest(component.TOU_ACCEPTED, postAcceptTOU),
   ];
 }
 
